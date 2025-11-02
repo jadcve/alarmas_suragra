2ee42619594da24c41390898b09cf21168d18c96
José Alaín Díaz Carrero <jadcve@gmail.com>
2025-11-02 17:21:53 -0300

    refactor: Standardize MSSQL_DB env var in echo-cfg.js

--- scripts/echo-cfg.js
+++ scripts/echo-cfg.js
@@ -3,7 +3,7 @@ console.log({
   DB_SOURCE: process.env.DB_SOURCE,
   MSSQL_SERVER: process.env.MSSQL_SERVER,
   MSSQL_HOST: process.env.MSSQL_HOST,
-  MSSQL_DATABASE: process.env.MSSQL_DATABASE,
+  MSSQL_DB: process.env.MSSQL_DB,
   MSSQL_USER: process.env.MSSQL_USER,
   MSSQL_PORT: process.env.MSSQL_PORT
 });
